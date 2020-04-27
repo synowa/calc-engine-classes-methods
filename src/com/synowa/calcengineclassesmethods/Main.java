2b0ec5bed03279431508e1f34da8810de20ab203
Aleksandra Pawlak <synowa8@gmail.com>
2020-04-27 22:46:01 +0200

    CalcEngine with classes and methods.

---
+++ src/com/synowa/calcengineclassesmethods/Main.java
@@ -0,0 +1,39 @@
+package com.synowa.calcengineclassesmethods;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+//        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+//        char[] opCodes = {'d', 'a', 's', 'm'};
+//        double[] results = new double[opCodes.length]; //this one and these three above we call parallel arrays
+
+
+        MathEquation[] equations = new MathEquation[4];
+        //equations[0] = new MathEquation();
+        equations[0] = create(100.0d, 50.0d, 'd');
+        equations[1] = create(25.0d, 92.0d, 'a');
+        equations[2] = create(225.0d, 17.0d, 's');
+        equations[3] = create(11.0d, 3.0d, 'm');
+
+        for (MathEquation equation : equations) {
+            equation.execute();
+            System.out.print("result = ");
+            System.out.println(equation.result);
+        }
+    }
+
+    public static MathEquation create (double leftVal, double rightVal, char opCode){
+       MathEquation equation = new MathEquation();
+       equation.leftVal = leftVal;
+       equation.rightVal = rightVal;
+       equation.opCode = opCode;
+
+       return equation;
+
+
+
+    }
+}
